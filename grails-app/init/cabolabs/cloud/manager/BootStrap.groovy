c146edd7f1c86058669fec9df492beb05c520cf2
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-11-25 01:17:51 -0300

    added more sample data

--- grails-app/init/cabolabs/cloud/manager/BootStrap.groovy
+++ grails-app/init/cabolabs/cloud/manager/BootStrap.groovy
@@ -3,6 +3,7 @@ package cabolabs.cloud.manager
 import com.cabolabs.cloud.accounts.*
 import com.cabolabs.cloud.resources.*
 import com.cabolabs.cloud.plans.*
+import com.cabolabs.cloud.subscription.*
 
 class BootStrap {
 
@@ -25,18 +26,40 @@ class BootStrap {
 
        resources*.save()
 
-// TODO: calculate priceDailyFraction from billingPeriod and price
+       // calculates priceDailyFraction from billingPeriod and price
        def plans = [
-          new Plan(name:'Bronze', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:10.0,  priceDailyFraction:0.333, resource:resources[0]),
-          new Plan(name:'Silver', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:25.0,  priceDailyFraction:0.833, resource:resources[0]),
-          new Plan(name:'Gold',   isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:100.0, priceDailyFraction:3.333, resource:resources[0]),
+          new Plan(name:'Bronze', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:10.0,  resource:resources[0]),
+          new Plan(name:'Silver', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:25.0,  resource:resources[0]),
+          new Plan(name:'Gold',   isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:100.0, resource:resources[0]),
 
-          new Plan(name:'Bronze', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:15.0,  priceDailyFraction:0.5, resource:resources[1]),
-          new Plan(name:'Silver', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:30.0,  priceDailyFraction:1.0, resource:resources[1]),
-          new Plan(name:'Gold',   isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:120.0, priceDailyFraction:4.0, resource:resources[1])
+          new Plan(name:'Bronze', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:15.0,  resource:resources[1]),
+          new Plan(name:'Silver', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:30.0,  resource:resources[1]),
+          new Plan(name:'Gold',   isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:120.0, resource:resources[1])
        ]
 
-       plans*.save()
+       //plans*.save()
+       plans.each {
+         if (!it.save()) println it.errors
+       }
+
+       // subscriber associated to a plan
+       def subscribers = [
+          new SubscriberAccount(companyName:'Client1', companyUrl:'http://client1.com', companyAddress1:'123 St. Thomas', country:'US')
+       ]
+
+       subscribers*.save()
+
+       def payment_info = new SubscriberPaymentConfig(gateway:'stripe', method:"debit/credit").save()
+
+       // created 100 days ago
+       def subscription = PlanAssociation(since:new Date()-100, status:'ACTIVE', plan:plans[0], subscriber:subscribers[0], payments:payment_info).save()
+
+       // TODO: sample transactions, one per month since 100 days ago, so we have 3 completed and one running (calculated)
+       def transactions = []
+
+       // TODO: need a process that calculates charging days from the billing period, and since, and the amount from priceDailyFraction if the period was not reached.
+       // use those calculated charging dates as the transaction dates for the transactions above ^
+
     }
     def destroy = {
     }
