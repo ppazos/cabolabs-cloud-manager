311ceaa52ae0096b8b11de34f1dd2cf44619fc46
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-11-25 01:17:02 -0300

    small refactor

--- grails-app/domain/com/cabolabs/cloud/subscription/PlanAssociation.groovy
+++ grails-app/domain/com/cabolabs/cloud/subscription/PlanAssociation.groovy
@@ -3,15 +3,21 @@ package com.cabolabs.cloud.plans
 import com.cabolabs.cloud.accounts.*
 import com.cabolabs.cloud.transactions.*
 
+/**
+ * Represents the subscription of a subscriber to a resource.
+ */
 class PlanAssociation {
 
    Date since
-   Date util
+   Date until
    String status
    BigDecimal balance
 
    SubscriberAccount subscriber
    Plan plan
+
+   List payments = []
+   static hasMany = [payments:SubscriberPaymentConfig]
 
    static constraints = {
       until nullable: true
