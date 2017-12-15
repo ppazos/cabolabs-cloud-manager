28c373cfc3d9dc6aaf10bdbbda932b2b93c050d3
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-12-15 02:37:59 -0300

    fixed bootstrap

--- grails-app/init/cabolabs/cloud/manager/BootStrap.groovy
+++ grails-app/init/cabolabs/cloud/manager/BootStrap.groovy
@@ -5,79 +5,132 @@ import com.cabolabs.cloud.resources.*
 import com.cabolabs.cloud.plans.*
 import com.cabolabs.cloud.subscription.*
 import com.cabolabs.cloud.transactions.*
+import com.cabolabs.cloud.security.*
 
 class BootStrap {
 
-    def init = { servletContext ->
+   def init = { servletContext ->
 
-       def publisher_accounts = [
-          new PublisherAccount(companyName:'CaboLabs', companyUrl:'https://www.cabolabs.com', companyAddress1:'Juan Paullier 995/703', country:'UY'),
-          new PublisherAccount(companyName:'Veratech', companyUrl:'https://www.cabolabs.com', companyAddress1:'Juan Paullier 995/703', country:'UY'),
-          new PublisherAccount(companyName:'GLYMS', companyUrl:'https://www.cabolabs.com', companyAddress1:'Juan Paullier 995/703', country:'UY'),
-          new PublisherAccount(companyName:'VirtualBox', companyUrl:'https://www.cabolabs.com', companyAddress1:'Juan Paullier 995/703', country:'UY'),
-          new PublisherAccount(companyName:'InfoClinic', companyUrl:'https://www.cabolabs.com', companyAddress1:'Juan Paullier 995/703', country:'UY'),
+       // ================================================
+       // Test users
+       def users = [
+          new User(username:'admin1', password:'admin1'),
+          new User(username:'publisher1', password:'publisher1'),
+          new User(username:'publisher2', password:'publisher2'),
+          new User(username:'subscriber1', password:'subscriber1'),
+          new User(username:'subscriber2', password:'subscriber2')
        ]
 
-       publisher_accounts*.save()
+       users*.save()
 
-       def resources = [
-          new Resource(name:'CloudEHRServer', isPublished:true, publisher:publisher_accounts[0], appUrl:'https://server001.cloudehrserver.com', marketingUrl:'https://cloudehrserver.com'),
-          new Resource(name:'Psy.Notes', isPublished:true, publisher:publisher_accounts[0], appUrl:'https://server001.psynotes.com', marketingUrl:'https://psynotes.com')
+       def roles = [
+          new Role(authority:'admin'),
+          new Role(authority:'publisher'),
+          new Role(authority:'subscriber')
        ]
 
-       resources*.save()
+       roles*.save()
 
-       // calculates priceDailyFraction from billingPeriod and price
-       def plans = [
-          new Plan(name:'Bronze', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:10.0,  resource:resources[0]),
-          new Plan(name:'Silver', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:25.0,  resource:resources[0]),
-          new Plan(name:'Gold',   isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:100.0, resource:resources[0]),
-
-          new Plan(name:'Bronze', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:15.0,  resource:resources[1]),
-          new Plan(name:'Silver', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:30.0,  resource:resources[1]),
-          new Plan(name:'Gold',   isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:120.0, resource:resources[1])
+       // Assign roles to users
+       def user_roles = [
+          new UserRole(user:users[0], role:roles[0]),
+          new UserRole(user:users[1], role:roles[1]),
+          new UserRole(user:users[2], role:roles[1]),
+          new UserRole(user:users[3], role:roles[2]),
+          new UserRole(user:users[4], role:roles[2])
        ]
 
-       //plans*.save()
-       plans.each {
-         if (!it.save()) println it.errors
-       }
+       user_roles*.save()
+       // ================================================
 
-       // subscriber associated to a plan
-       def subscribers = [
-          new SubscriberAccount(companyName:'Client1', companyUrl:'http://client1.com', companyAddress1:'123 St. Thomas', country:'US')
-       ]
 
-       subscribers*.save()
+      // Test accounts, resources and plans
+      def publisher_accounts = [
+        new PublisherAccount(contact: user_roles[1], companyName:'CaboLabs',
+                             companyUrl:'https://www.cabolabs.com',
+                             companyAddress1:'Juan Paullier 995/703', country:'UY'),
+        new PublisherAccount(contact: user_roles[2], companyName:'Veratech',
+                             companyUrl:'https://www.cabolabs.com',
+                             companyAddress1:'Juan Paullier 995/703', country:'UY')
+        //,
+        //new PublisherAccount(companyName:'GLYMS', companyUrl:'https://www.cabolabs.com', companyAddress1:'Juan Paullier 995/703', country:'UY'),
+        //new PublisherAccount(companyName:'VirtualBox', companyUrl:'https://www.cabolabs.com', companyAddress1:'Juan Paullier 995/703', country:'UY'),
+        //new PublisherAccount(companyName:'InfoClinic', companyUrl:'https://www.cabolabs.com', companyAddress1:'Juan Paullier 995/703', country:'UY'),
+      ]
 
-       def payment_info = new SubscriberPaymentConfig(gateway:'stripe', method:"debit/credit").save()
+      publisher_accounts*.save()
 
-       // created 100 days ago
-       def subscription = PlanAssociation(since:new Date()-100, status:'ACTIVE', plan:plans[0], subscriber:subscribers[0], payments:payment_info).save()
+      def resources = [
+        new Resource(name:'CloudEHRServer', isPublished:true, publisher:publisher_accounts[0], appUrl:'https://server001.cloudehrserver.com', marketingUrl:'https://cloudehrserver.com'),
+        new Resource(name:'Psy.Notes', isPublished:true, publisher:publisher_accounts[0], appUrl:'https://server001.psynotes.com', marketingUrl:'https://psynotes.com')
+      ]
 
-       def close_dates = subscription.billingCycleCloseDatesSince(100)
+      resources*.save()
+
+      // calculates priceDailyFraction from billingPeriod and price
+      def plans = [
+        new Plan(name:'Bronze', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:10.0,  resource:resources[0]),
+        new Plan(name:'Silver', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:25.0,  resource:resources[0]),
+        new Plan(name:'Gold',   isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:100.0, resource:resources[0]),
+
+        new Plan(name:'Bronze', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:15.0,  resource:resources[1]),
+        new Plan(name:'Silver', isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:30.0,  resource:resources[1]),
+        new Plan(name:'Gold',   isEnabled:true, billingPeriod:EBillingPeriod.MONTHLY, price:120.0, resource:resources[1])
+      ]
+
+      //plans*.save()
+      plans.each {
+       if (!it.save()) println it.errors
+      }
+
+      // =========================================================
+      // Subscribers and Subscriptions
+      // subscriber associated to a plan
+      def subscribers = [
+         new SubscriberAccount(companyName:'Client1', companyUrl:'http://client1.com',
+                               companyAddress1:'123 St. Thomas', country:'US',
+                               contact: user_roles[3])
+      ]
+
+      subscribers*.save()
+
+      def payment_info = new SubscriberPaymentConfig(gateway:'stripe', method:"debit/credit").save()
+
+      def subscription = new Subscription(since:new Date()-100,
+                                          status:'ACTIVE',
+                                          plan:plans[0],
+                                          subscriber:subscribers[0],
+                                          payments:payment_info)
+      if (!subscription.validate())
+      {
+         println subscription.errors
+      }
+
+      // created 100 days ago
+      //def subscription = new Subscription(since:new Date()-100, status:'ACTIVE', plan:plans[0], subscriber:subscribers[0], payments:payment_info).save()
+
+      def close_dates = subscription.getBillingCycleCloseDatesSince(100)
 
        // TODO: generate the transaction number per Plan, as a numeric string
        // TODO: sample transactions, one per month since 100 days ago, so we have 3 completed and one running (calculated)
-       def transactions = [
+      def transactions = [
 
-       ]
+      ]
 
-       def start, i = 1
-       close_dates.each {
-          start = it - subscription.plan.billingPeriod.value
-          transactions << new Transaction(type:'CHARGE', subscription:subscription,
-                                          number:'0000000000'+i,
-                                          billingPeriodFrom:start, billingPeriodTo:it)
-          i++
-       }
+      def start, i = 1
+      close_dates.each {
+         start = it - subscription.plan.billingPeriod.value
+         transactions << new Transaction(type:'CHARGE', subscription:subscription,
+                                         number:'0000000000'+i,
+                                         billingPeriodFrom:start, billingPeriodTo:it)
+         i++
+      }
 
-       transactions*.save()
+      transactions*.save()
 
-       // TODO: need a process that calculates charging days from the billing period, and since, and the amount from priceDailyFraction if the period was not reached.
-       // use those calculated charging dates as the transaction dates for the transactions above ^
-
-    }
-    def destroy = {
-    }
+      // TODO: need a process that calculates charging days from the billing period, and since, and the amount from priceDailyFraction if the period was not reached.
+      // use those calculated charging dates as the transaction dates for the transactions above ^
+   }
+   def destroy = {
+   }
 }
