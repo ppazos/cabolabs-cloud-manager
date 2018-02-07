b71357168eaaf7130a9654c64bae0a834b7e218d
Pablo Pazos <pablo.pazos@cabolabs.com>
2018-02-07 09:28:20 -0300

    added fields to user

--- grails-app/domain/com/cabolabs/cloud/security/User.groovy
+++ grails-app/domain/com/cabolabs/cloud/security/User.groovy
@@ -5,11 +5,12 @@ class User {
    String username
    String password
    Date dateCreated
+   String phone // business contact phone
+   String roleInOrganization // CTO, CEO, CIO, ...
    String role // TODO: mapping because is reserved work on dbms
 
    static constraints = {
       username unique: true
       role inList:['admin', 'publisher', 'subscriber'] // TODO: enum
-
    }
 }
