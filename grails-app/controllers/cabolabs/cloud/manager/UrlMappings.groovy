d3117bb6c2efd9042d2327aa3fc2864d03e2cff0
Pablo Pazos <pablo.pazos@cabolabs.com>
2018-02-08 00:19:04 -0300

    added login and pass reset views

--- grails-app/controllers/cabolabs/cloud/manager/UrlMappings.groovy
+++ grails-app/controllers/cabolabs/cloud/manager/UrlMappings.groovy
@@ -3,6 +3,11 @@ package cabolabs.cloud.manager
 class UrlMappings {
 
     static mappings = {
+       "/auth/reset"(
+          controller: "auth",
+          action: "resetPassword"
+       )
+
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
--- grails-app/controllers/com/cabolabs/cloud/security/AuthController.groovy
+++ grails-app/controllers/com/cabolabs/cloud/security/AuthController.groovy
@@ -17,17 +17,55 @@ class AuthController {
 
    }
 
+   // sends email because the user forgot his password
    def resetPasswordRequest(String email)
    {
+      if (!email)
+      {
+         flash.message = message(code:"user.resetPasswordRequest.noEmail")
+         redirect action:'login'
+         return
+      }
+
       def user = User.findByEmail(email)
 
       if (!user)
       {
-         flash.message = message(code:"user.forgotPassword.emailDoesntExists")
-         redirect(action:'show', id:params.id)
+         flash.message = message(code:"user.resetPasswordRequest.emailDoesntExists")
+         redirect(action:'login')
          return
       }
 
+      // TODO
+
+   }
+
+   // access from email to reset the password from a password reset view
+   def resetPassword(String token)
+   {
+      if (!token)
+      {
+         flash.message = message(code:"user.resetPassword.noToken")
+         redirect action:'login'
+         return
+      }
+
+      def user = User.findByResetPasswordToken(token)
+
+      if (!user)
+      {
+         flash.message = message(code:"user.resetPassword.invalidToken")
+         redirect(action:'login')
+         return
+      }
+
+      if (request.post)
+      {
+         // TODO: do pass reset
+         // https://github.com/ppazos/cabolabs-ehrserver/blob/master/grails-app/controllers/com/cabolabs/security/UserController.groovy#L670
+      }
+
+      // TODO: render password reset view
 
    }
 }
